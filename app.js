c75b340d219453a831df6276c97c1c579d92c905
gatra15 <gatra881@gmail.com>
2023-08-29 10:31:49 +0700

    update app.js

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/app2', (req, res) => {
   res.send('Hello this App 2!')
 });
 
+app.get('/gatra', (req, res) => {
+  res.send('Belajar Node Js IDN bersama Gatra ^_^')
+});
+
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
